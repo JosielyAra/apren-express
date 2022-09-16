8e957fce8216ba53454c373dfaa4b54e27ad1538
Josi <jtk.jpaf@gmail.com>
2022-09-15 22:14:55 -0400

    Mejorar codigo de creacion de servidor con express

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require("http");
+/*const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.status = 200;
@@ -7,5 +7,18 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000, () => {
+  console.log("Server on port 3000");
+});
+*/
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
   console.log("Server on port 3000");
 });
